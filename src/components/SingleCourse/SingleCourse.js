bf2071aed4c92d00b42ed3dea3dacca8767f7846
jhangkar <taqit3852@gmail.com>
2022-10-26 03:02:24 +0600

    trending founded

--- src/components/SingleCourse/SingleCourse.js
+++ src/components/SingleCourse/SingleCourse.js
@@ -4,11 +4,14 @@ import './SingleCourse.css'
 import { FaArrowRight, FaHeart } from "react-icons/fa";
 const SingleCourse = ({course}) => {
 
-    const {_id,title,picture,rating,intro} = course
+    const {_id,title,picture,rating,intro,isActive} = course
 
     return (
-        <div className='course-card p-6 border border-slate-900 relative'>
-            <div className='absolute inset-x-3/4 -inset-y-4'><p>trending</p></div>
+        <div className='course-card p-6 border border-slate-100 relative'>
+            {
+                isActive && <div className='absolute inset-x-3/4 -inset-y-1    w-24'><p className='font-medium text-center text-white shadow-lg  bg-red-600 rounded-3xl '>Trending</p></div>
+            }
+            
             <img src={picture} alt="" />
             <h1 className='text-xl font-semibold py-2'>{title}</h1>
             <p>{intro.lenght < 150 ? intro : intro.slice(0,150)}...</p>
