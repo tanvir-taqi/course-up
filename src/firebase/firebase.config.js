c64c12ff8c79164d497639f5edbe586f600c0768
jhangkar <taqit3852@gmail.com>
2022-10-25 16:59:09 +0600

    preoject set up , firebase,tailwind set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTyF3e2EdHPF4SHYk-QbBQnF1OD_Sg4VE",
+  authDomain: "courseup-88a46.firebaseapp.com",
+  projectId: "courseup-88a46",
+  storageBucket: "courseup-88a46.appspot.com",
+  messagingSenderId: "464072166567",
+  appId: "1:464072166567:web:0a030d6ee4281486495f82"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
